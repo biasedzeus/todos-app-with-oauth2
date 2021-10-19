d948798802290c5143a97e468badd1d4abc95e14
biasedzeus <bhanu.singh1078@gmail.com>
2021-10-19 20:44:33 +0530

    Homepage added
    basic homepage,changes to be done later

---
+++ src/Pages/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Todos</h1>
+            <button className="homepage-buttons">Login</button>
+           <button className="homepage-buttons">Sign Up</button>
+
+            
+        </div>
+    )
+}
+
+export default HomePage
